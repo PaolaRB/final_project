4c0d577dfbcd6eabd6d1b630caebab7dadf338cc
Paola R <paolin25@gmail.com>
2019-06-14 20:28:56 -0400

    Work in progress

--- README.md
+++ README.md
@@ -19,20 +19,37 @@ Your repository should include the following:
 
 ## Research Question
 
-Based on the 30 attributes/features from digitized images of a breast mass, can we predict the diagnosis if it is Benign or Malignant?
+From the 30 atributes of breast cancer dataset, which attributes have more correlation and which not, and also which prediction model 
+between PCA, KNN and Logistic Regression has more accuracy to predict the diagnosis.
 
 ### Abstract
 
-4 sentence longer explanation about your research question. Include:
-
-- opportunity (what data do we have)
-- challenge (what is the "problem" we could solve with this dataset)
-- action (how will we try to solve this problem/answer this question)
-- resolution (what did we end up producing)
+Derived from UCI Machine Learning Repository, a brest cancer dataset is available and it represents the features computed 
+from digitized image of a fine needle aspirate (FNA) of a breast mass.
+Using these dataset, the understanding of this characteristics and its relationships could impact in the prediction of malignant or bening cancer. 
+To achieve this I have used some machine learning classification methods to fit a function that can predict the discrete class of new input. 
 
 ### Introduction
 
-Brief (no more than 1-2 paragraph) description about the dataset. Can copy from elsewhere, but cite the source (i.e. at least link, and explicitly say if it's copied from elsewhere).
+###### Identify the problem
+
+Breast cancer is the most common cancer among canadian women and it is the second leading cause of death from cancer according to Canadian Cancer Society. 
+Some statistics from 2017 are: 
+- 26,300 women were diagnosed with breast cancer and that was 25% of all new cancer in woman in 2017. 
+- 5,000 women died from breast cancer which represents 13% of all cancer deaths in women. 
+- An average of 72 canadian women were diagnosed from breast cancer every day.
+- An average 14 canadian woman died from breast cancer every day.
+
+The rate of incidence of breast cancer in Canada rose in the early of 90's but decreased in the early of 2000's and one of the reason was the use of mamography. 
+In addtion the death rate has been declining since the mid of 80's and this is the reflect of the improvement of screening and imaging.(*) 
+
+###### Analysing the dataset
+
+`Bar diagram by` [View](./figures/AllGroupbyDiagnosis.png)
+
+`Correlation diagram` [View](./figures/1-pairplot-hist-mean.png)
+
+For example: Diagram of the feature Concavity with its Mean, Error and Worst attributes [View](./figures/concavity-Mean-Error-Worst.png)
 
 ### Methods
 
@@ -57,5 +74,19 @@ Brief (no more than 1-2 paragraph) description about what you did. Include:
 
 ### References
 All of the links
+(*) https://www.cancer.ca/en/cancer-information/cancer-type/breast/statistics/?region=on
 
--------
+## Data
+
+The data set is from the "Breast Cancer Wisconsin (Diagnostic) Database" freely available in python's sklearn library, for details see:  
+https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+%28Diagnostic%29
+
+* Number of Samples: 569  
+* Number of Features: 30 numeric, predictive attributes  
+* Number of Classes: 2 
+
+Features are computed from a digitized image of a fine needle aspirate (FNA) of a breast mass. They describe characteristics of the cell nuclei present in the image. Ten real-valued features are computed for each cell nucleus. The mean, standard error and 'worst' or largest (mean of the three largest values) of these features were computed for each image, resulting in 30 features. For instance, the radius measurements are for the 'mean radius',  'standard error of the radius', and 'worst radius'. All feature values are recoded with four significant digits.
+
+The two target classes correspond to negative outcomes (Benign) and positive outcomes (Malignant).
+
+**This original data set will be randomly split into two sets for train and test purposes.**
--- classification_analysis.py
+++ classification_analysis.py
@@ -8,7 +8,10 @@ from sklearn.metrics import classification_report, confusion_matrix, f1_score
 from sklearn.model_selection import cross_val_score, ShuffleSplit
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+os.makedirs('figures', exist_ok=True)
 
 cancer = load_breast_cancer()
 X = cancer.data
@@ -26,107 +29,179 @@ print(f'keys: {cancer.keys()}')
 tmp = np.c_[cancer.data, cancer.target]
 cancer_df = pd.DataFrame(tmp, columns=feature_names)
 print(cancer_df.head())
+CONST_B = 1; CONST_M = 0
+cancer_df['diagnosis_ds'] = cancer_df['target'].map({CONST_B: 'Benign', CONST_M: 'Malignant'})
+base_columns = ['radius', 'texture', 'perimeter', 'area', 'smoothness', 'compactness', 'concavity', 'concave points', 'symmetry', 'fractal dimension']
 
-# ************************************
-# ********** Applying PCA ************
-# ************************************
-scaler = StandardScaler()
-scaler.fit(X)
-X_scaled = scaler.transform(X)
-print(X_scaled)
-
-pca = PCA(n_components=30)
-pca.fit(X_scaled)
-X_pca = pca.transform(X_scaled)
-
-print('shape of X_pca', X_pca.shape)
-expl = pca.explained_variance_ratio_
-print(expl)
-print('sum  8: ', sum(expl[0:8]))
-print('sum 10: ', sum(expl[0:10]))
-print('sum 12: ', sum(expl[0:12]))
-print('sum 15: ', sum(expl[0:15]))
-print('sum 20: ', sum(expl[0:20]))
-
-plt.plot(np.cumsum(pca.explained_variance_ratio_))
-plt.xlabel('number of components')
-plt.ylabel('cumulative explained variance')
-plt.show()
-
-Xax = X_pca[:, 0]
-Yax = X_pca[:, 1]
-labels = cancer_df['target'].values
-cdict = {0: 'red', 1: 'green'}
-labl = {0: 'Malignant', 1: 'Benign'}
-marker = {0: 'o', 1: '*'}
-alpha = {0: .3, 1: .5}
-fig, ax = plt.subplots(figsize=(7, 5))
-fig.patch.set_facecolor('white')
-for l in np.unique(labels):
-    ix = np.where(labels == l)
-    ax.scatter(Xax[ix], Yax[ix], c=cdict[l], label=labl[l], s=40, marker=marker[l], alpha=alpha[l])
-
-plt.xlabel("First Principal Component", fontsize=14)
-plt.ylabel("Second Principal Component", fontsize=14)
-plt.legend()
-plt.show()
-
-# ************************************
-# ********** Split dataset ***********
-# ************************************
-
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.35)
-
-# Printing splitted datasets
-print(f'X_train.shape : {X_train.shape}, y_train.shape : {y_train.shape}')
-print(f'X_test.shape : {X_test.shape}, y_test.shape : {y_test.shape}')
-
-# ************************************
-# ********** Training model***********
-# ************************************
-
-lr = LogisticRegression(solver='lbfgs', multi_class='auto', max_iter=4000)
-lr.fit(X_train, y_train)
-
-print(f'Intercept per class: {lr.intercept_}\n')
-print(f'Coeficients per class: {lr.coef_}\n')
-print(f'Available classes : {lr.classes_}\n')
-print(f'Named Coeficients for class 0: {pd.DataFrame(lr.coef_[0], cancer.feature_names)}\n')
-print(f'Number of iterations generating model : {lr.n_iter_}')
-
-# from sklearn.externals import joblib
-# joblib.dump(lr, 'model.pkl')
-
-# ************************************
-# ********** Predicting the results **
-# ************************************
-
-predicted_values = lr.predict(X_test)
-
-for (real, predicted) in list(zip(y_test, predicted_values)):
-    print(f'Value: {real}, pred:{predicted} {"is different " if real != predicted else ""}')
-
-# ************************************
-# ********** Accuracy SCore **********
-# ************************************
-print(f'Accuracy score is {lr.score(X_test, y_test):.2f}/1 \n')
-
-# ************************************
-# ******** Printing Reports **********
-# ************************************
-print('Classification report\n')
-print(classification_report(y_test, predicted_values))
-print('Confusion matrix\n')
-print(confusion_matrix(y_test, predicted_values))
-print('Overal f1-score\n')
-print(f1_score(y_test, predicted_values, average="macro"))
+def get_ax(df, column_name, suffix, x, label_group):
+    if suffix == "error":
+        s_column = f'{column_name} {suffix}'
+    else:
+        s_column = f'{suffix} {column_name}'
+    sns.distplot(df[df['target'] == CONST_B][s_column], bins=10, kde=False, ax=axes[x, 0],
+                 color='royalblue', label='Benign')
+    sns.distplot(df[df['target'] == CONST_M][s_column], bins=10, kde=False, ax=axes[x, 0],
+                 color='orange', label='Malignant')
+    axes[x, 0].set_xlabel(column_name + ': Benign and Malignant')
+    axes[x, 0].set_ylabel(label_group)
+    #
+    sns.distplot(df[df['target'] == CONST_B][s_column], bins=10, kde=False, ax=axes[x, 1],
+                 color='royalblue', label='Benign')
+    axes[x, 1].set_xlabel(column_name + ': Benign')
+    #
+    sns.distplot(df[df['target'] == CONST_M][s_column], bins=10, kde=False, ax=axes[x, 2],
+                 color='orange', label='Malignant')
+    axes[x, 2].set_xlabel(column_name + ': Malignant')
+    #
+    sns.boxplot(x=df['diagnosis_ds'], y=df[s_column], ax=axes[x, 3], order=['Benign', 'Malignant'])
+    axes[x, 3].set_xlabel('Diagnosis')
+    axes[x, 3].set_ylabel("")
+    return axes[x, 3]
 
 
-# ************************************
-# ******** Cross Validation **********
-# ************************************
-print(f'Cross_val_score before ShuffleSplit')
-print(cross_val_score(lr, X, y, cv=10))
-cv = ShuffleSplit(n_splits=5)
-print(f'Cross_val_score after ShuffleSplit')
-print(cross_val_score(lr, X, y, cv=cv))
+for name_base in base_columns:
+    fig, axes = plt.subplots(3, 4, figsize=(18, 12))
+    fig.suptitle(f'Analysis {name_base} : Mean, Error, Worst', fontsize=12)
+    # ====
+    # Mean
+    # ====
+    get_ax(cancer_df, column_name=name_base, suffix='mean', x=0, label_group="Mean")
+    # ====
+    # Error
+    # ====
+    get_ax(cancer_df, column_name=name_base, suffix='error', x=1, label_group="Error")
+    # ====
+    # Worst
+    # ====
+    get_ax(cancer_df, column_name=name_base, suffix='worst', x=2, label_group="Worst")
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(f'figures/{name_base}-Mean-Error-Worst.png')
+    plt.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+# # ************************************
+# # ********** Applying PCA ************
+# # ************************************
+# scaler = StandardScaler()
+# scaler.fit(X)
+# X_scaled = scaler.transform(X)
+# print(X_scaled)
+#
+# pca = PCA(n_components=30)
+# pca.fit(X_scaled)
+# X_pca = pca.transform(X_scaled)
+#
+# print('shape of X_pca', X_pca.shape)
+# expl = pca.explained_variance_ratio_
+# print(expl)
+# print('sum  8: ', sum(expl[0:8]))
+# print('sum 10: ', sum(expl[0:10]))
+# print('sum 12: ', sum(expl[0:12]))
+# print('sum 15: ', sum(expl[0:15]))
+# print('sum 20: ', sum(expl[0:20]))
+#
+# plt.plot(np.cumsum(pca.explained_variance_ratio_))
+# plt.xlabel('number of components')
+# plt.ylabel('cumulative explained variance')
+# plt.show()
+#
+# Xax = X_pca[:, 0]
+# Yax = X_pca[:, 1]
+# labels = cancer_df['target'].values
+# cdict = {0: 'red', 1: 'green'}
+# labl = {0: 'Malignant', 1: 'Benign'}
+# marker = {0: 'o', 1: '*'}
+# alpha = {0: .3, 1: .5}
+# fig, ax = plt.subplots(figsize=(7, 5))
+# fig.patch.set_facecolor('white')
+# for l in np.unique(labels):
+#     ix = np.where(labels == l)
+#     ax.scatter(Xax[ix], Yax[ix], c=cdict[l], label=labl[l], s=40, marker=marker[l], alpha=alpha[l])
+#
+# plt.xlabel("First Principal Component", fontsize=14)
+# plt.ylabel("Second Principal Component", fontsize=14)
+# plt.legend()
+# plt.show()
+#
+# # ************************************
+# # ********** Split dataset ***********
+# # ************************************
+#
+# X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.35)
+#
+# # Printing splitted datasets
+# print(f'X_train.shape : {X_train.shape}, y_train.shape : {y_train.shape}')
+# print(f'X_test.shape : {X_test.shape}, y_test.shape : {y_test.shape}')
+#
+# # ************************************
+# # ********** Training model***********
+# # ************************************
+#
+# lr = LogisticRegression(solver='lbfgs', multi_class='auto', max_iter=4000)
+# lr.fit(X_train, y_train)
+#
+# print(f'Intercept per class: {lr.intercept_}\n')
+# print(f'Coeficients per class: {lr.coef_}\n')
+# print(f'Available classes : {lr.classes_}\n')
+# print(f'Named Coeficients for class 0: {pd.DataFrame(lr.coef_[0], cancer.feature_names)}\n')
+# print(f'Number of iterations generating model : {lr.n_iter_}')
+#
+# # from sklearn.externals import joblib
+# # joblib.dump(lr, 'model.pkl')
+#
+# # ************************************
+# # ********** Predicting the results **
+# # ************************************
+#
+# predicted_values = lr.predict(X_test)
+#
+# for (real, predicted) in list(zip(y_test, predicted_values)):
+#     print(f'Value: {real}, pred:{predicted} {"is different " if real != predicted else ""}')
+#
+# # ************************************
+# # ********** Accuracy SCore **********
+# # ************************************
+# print(f'Accuracy score is {lr.score(X_test, y_test):.2f}/1 \n')
+#
+# # ************************************
+# # ******** Printing Reports **********
+# # ************************************
+# print('Classification report\n')
+# print(classification_report(y_test, predicted_values))
+# print('Confusion matrix\n')
+# print(confusion_matrix(y_test, predicted_values))
+# print('Overal f1-score\n')
+# print(f1_score(y_test, predicted_values, average="macro"))
+#
+#
+# # ************************************
+# # ******** Cross Validation **********
+# # ************************************
+# print(f'Cross_val_score before ShuffleSplit')
+# print(cross_val_score(lr, X, y, cv=10))
+# cv = ShuffleSplit(n_splits=5)
+# print(f'Cross_val_score after ShuffleSplit')
+# print(cross_val_score(lr, X, y, cv=cv))
